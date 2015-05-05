045856f4a184561bba6cf78a3bf0e1a71713e758
Nial Francis <Nial.Francis@globalx.com.au>
2015-05-05 18:20:34 +1000

    Fixed:
    Force switch
    History logic
    Confirmation message
    Made fix function private
    Alias works

--- PoShFuck.psm1
+++ PoShFuck.psm1
@@ -7,39 +7,53 @@
 	.EXAMPLE
 	Fuck
 #>
+
 [CmdletBinding()]
-#param
-#(
-#    [Parameter(Mandatory=$False, ValueFromPipeline=$False, ValueFromPipelineByPropertyName=$True, HelpMessage='')] [switch]$Yes
-#)
+param
+(
+	[Parameter(Mandatory=$False, ValueFromPipeline=$False, ValueFromPipelineByPropertyName=$True)]
+	[switch]$Force
+)
 
-    $lastcommand = Get-History -Count 1
-    $lasterror = $Error[0]
-
-    $newcommand = Fuck-Fix($lastcommand.CommandLine, $lasterror)
-
-    $Force = $False    #REMOVE THIS WHEN FORCE IS FIXED
-    if ($Force) { Invoke-Expression "$newcommand"; }
-    else
-    {
-        $title = "Fuck!"
-        $message = "Did you mean: $xxxx?"
-        $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes",'Execute'
-        $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No",'Exit'
-        $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
-        $answer = $host.ui.PromptForChoice($title, $message, $options, 0)
-
-        if ($answer -eq 0) { Invoke-Expression "$newcommand"; }
-    }
-    
+	## GATHER THE LAST COMMAND, BUT NOT IF IT IS ITSELF
+	$cmditeration = 1
+	do {
+		$lastcommand = (Get-History -Count $cmditeration)[0]
+		$aliascmd = $lastcommand.CommandLine.Split(' ')[0]
+		Write-Verbose "
+			Testing executed command: $lastcommand
+			Testing resolved command: $($(get-alias $aliascmd -ea SilentlyContinue).ResolvedCommand.Name)"
+		$cmditeration++
+	} until (
+		(($lastcommand.CommandLine -notmatch "Invoke-TheFuck") -and (((get-alias $aliascmd -ea SilentlyContinue).ResolvedCommand.Name) -notmatch "Invoke-TheFuck")) -or
+		($lastcommand -eq $null)
+	)
+## TODO: TEST THIS
+	if ($lastcommand -eq $null) { throw "Cannot fuck without a previous command" }
+	
+	Write-Verbose "Fucking command: $lastcommand"
+	
+	$lasterror = $Error[0]
+	
+	$newcommand = FuckFix($lastcommand.CommandLine, $lasterror)
+	
+	if ($Force) { Invoke-Expression "$newcommand"; }
+	else
+	{
+		$title = "Did you mean?"
+		$message = " $newcommand"
+		$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes",'Execute'
+		$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No",'Exit'
+		$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+		$answer = $host.ui.PromptForChoice($title, $message, $options, 0)
+	
+		if ($answer -eq 0) { Invoke-Expression "$newcommand"; }
+	}
 
 }
-Set-Alias Fuck Invoke-TheFuck
-#Set-Alias Fuck! Invoke-TheFuck -Yes
 
+Function FuckFix ($cmd, $error) {
 
-Function Fuck-Fix ($cmd, $error)
-{
 	#Bunch of stand-alone IF blocks (not a switch) so it can hit multiple conditions and be corrected multiple times
 	
     #cleanup the command
@@ -57,3 +71,8 @@ Function Fuck-Fix ($cmd, $error)
     return $cmd
 
 }
+
+export-modulemember *-*
+
+Set-Alias -Scope global -Name "Fuck" -Value "Invoke-TheFuck"
+Set-Alias -Scope global -Name "Fuck!" -Value "Invoke-TheFuck -Force"
