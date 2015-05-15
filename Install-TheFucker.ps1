f09c77a615ff625ac4df7faa7e1cce33417f9507
Serhiy Kalinets <kalinets@gmail.com>
2015-05-15 13:40:00 +0300

    fixes error in install script

--- Install-TheFucker.ps1
+++ Install-TheFucker.ps1
@@ -11,6 +11,6 @@ Move-Item "$dst\PoShFuck-master\*" "$dst" -Force
 Remove-Item "$dst\PoShFuck-master" -Recurse -Force
 Remove-Item $pfk -Force
 
-if ( !(Select-String -Path $profile -Pattern "Import-Module PoShFuck") {
+if ( !(Select-String -Path $profile -Pattern "Import-Module PoShFuck")) {
 	Write-Output "Import-Module PoShFuck" | Out-File $profile -Append
 }
