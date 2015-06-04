8b029d714f1557e5ca8a1e111d73293adfc86ca1
mattparkes <mattparkes@null.net>
2015-06-04 15:06:36 +1000

    Update Install-TheFucker.ps1
    Pokemon catch the exceptions when installing.

--- Install-TheFucker.ps1
+++ Install-TheFucker.ps1
@@ -11,6 +11,13 @@ Move-Item "$dst\PoShFuck-master\*" "$dst" -Force
 Remove-Item "$dst\PoShFuck-master" -Recurse -Force
 Remove-Item $pfk -Force
 
-if ( !(Select-String -Path $profile -Pattern "Import-Module PoShFuck")) {
-	Write-Output "Import-Module PoShFuck" | Out-File $profile -Append -encoding utf8
+try
+{
+	if ( !(Select-String -Path $profile -Pattern "Import-Module PoShFuck")) {
+		Write-Output "Import-Module PoShFuck" | Out-File $profile -Append -encoding utf8
+	}
+}
+catch [system.exception]
+{
+	Write-Output "Import-Module PoShFuck" | Out-File $profile -Force -encoding utf8
 }
